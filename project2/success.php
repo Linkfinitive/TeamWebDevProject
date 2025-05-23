2be67c47ba45a5c8fb78f20705a978cffc371355
Linkfinitive <116618526+Linkfinitive@users.noreply.github.com>
2025-05-23 11:15:24 +1000

    Added validation to ensure that the success page comes from the link, not by typing it in

--- project2/success.php
+++ project2/success.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
-<?php
-    include 'header.inc';
-?>
+
+<?php if (!$_SERVER["HTTP_REFERER"]) {
+    header("Location: error.php");
+    exit();
+} ?>
+
+<?php include "header.inc"; ?>
 
 <body>
     <p>Your item was submited successfully!</p>
     <p>Your EOI is: </p>
 </body>
 
-<?php
-    include 'footer.inc';
-?>
+<?php include "footer.inc"; ?>
+
+
