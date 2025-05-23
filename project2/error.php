3c2bfa177d9fff3967d4ff495ad3b41da3260222
vidhianand33 <104094557@student.swin.edu.au>
2025-05-23 13:57:09 +1000

    base code for error page

--- project2/error.php
+++ project2/error.php
@@ -1,4 +1,33 @@
 <!-- This will be a generic error page that the user is sent to
 every time there is something wrong with the connection, form data,
-etc. It should be structuered like our other pages (header, footer, etc included)
+etc. It should be structured like our other pages (header, footer, etc included)
 and contain a simple "something went wrong. please try again later" message. -->
+
+<?php
+// error.php – A generic error page
+?>
+
+<!doctype html>
+
+<?php
+  include 'header.inc';
+?>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="author" content="TeamWebDev" />
+    <meta name="description" content="errorpage" />
+    <meta name="keywords" content="Assignment 1" />
+    <link rel="stylesheet" href="./styles/styles.css" />
+    <link rel="stylesheet" href="./styles/layout.css" />
+
+<body>
+    <div class="error-container">
+        <h1>Oops! Something went wrong.</h1>
+        <p>Please try again later.</p>
+    </div>
+
+    <?php include_once('footer.inc'); ?>
+</body>
+</html>
