5f91b65dcfa2d31c3bcd6d4e7b10173c901d409b
Linkfinitive <116618526+Linkfinitive@users.noreply.github.com>
2025-05-25 12:45:13 +1000

    Created the manager page

---
+++ project2/manager.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<?php
+  include 'header.inc';
+?>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="author" content="TeamWebDev" />
+    <meta name="description" content="homepage" />
+    <meta name="keywords" content="Assignment 1" />
+    <link rel="stylesheet" href="./styles/styles.css" />
+    <link rel="stylesheet" href="./styles/layout.css" />
+
+    <title>EOI Manager</title>
+  </head>
+  <body>
+    This is the manager page yay
+  </body>
+</html>
+
+<?php
+include 'footer.inc';
+?>
