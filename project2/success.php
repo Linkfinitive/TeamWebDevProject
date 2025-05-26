c387678533fb52bc53c6f5ec8b2ce9d51c901271
Linkfinitive <116618526+Linkfinitive@users.noreply.github.com>
2025-05-26 11:50:36 +1000

    Removed the code that broke the website

--- project2/success.php
+++ project2/success.php
@@ -13,10 +13,12 @@
 </body>
 
 <?php
-    require_once 'process_eoi.php';
-    $result = mysqli_query($conn,"SELECT MAX(id) AS id FROM eoi");
-    $eoid = mysqli_fetch_row($result);
-    echo[$eoid];
+//I removed this because it broke the website - this page can't call process_eoi because it will have already been executed. Did you mean require_once "settings.php"; ?
+
+    // require_once 'process_eoi.php';
+    // $result = mysqli_query($conn,"SELECT MAX(id) AS id FROM eoi");
+    // $eoid = mysqli_fetch_row($result);
+    // echo[$eoid];
 ?>
 
 <?php include "footer.inc"; ?>
