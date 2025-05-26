9539ff8417447c2b5feb98bbb1d8f5e73d39eb3a
Linkfinitive <116618526+Linkfinitive@users.noreply.github.com>
2025-05-26 12:01:41 +1000

    Styled the success and error pages

--- project2/error.php
+++ project2/error.php
@@ -18,15 +18,17 @@ and contain a simple "something went wrong. please try again later" message. -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="author" content="TeamWebDev" />
     <meta name="description" content="errorpage" />
-    <meta name="keywords" content="Assignment 1" />
+    <meta name="keywords" content="Assignment 2" />
     <link rel="stylesheet" href="./styles/styles.css" />
     <link rel="stylesheet" href="./styles/layout.css" />
 
 <body>
+  <main class="main">
     <div class="error-container">
         <h1>Oops! Something went wrong.</h1>
         <p>Please try again later.</p>
     </div>
+  </main>
 
     <?php include_once('footer.inc'); ?>
 </body>
