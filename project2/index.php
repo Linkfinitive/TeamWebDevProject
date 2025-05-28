978b0139ea1bc37492e0652422c6005cad3a686f
Linkfinitive <116618526+Linkfinitive@users.noreply.github.com>
2025-05-28 19:19:51 +1000

    Added homepage description

--- project2/index.php
+++ project2/index.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="author" content="TeamWebDev" />
-    <meta name="description" content="homepage" />
+    <meta name="description" content="The Homepage of the Website" />
     <meta name="keywords" content="Assignment 1" />
     <link rel="stylesheet" href="./styles/styles.css" />
     <link rel="stylesheet" href="./styles/layout.css" />
